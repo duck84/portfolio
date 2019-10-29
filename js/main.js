5d25cf05a4bae111c1df790db7be245d875715b0
Mike <mmcgrath@psu.edu>
2019-10-29 15:16:10 -0700

    Added js to link to projects' repositories

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const buttonTower = document.querySelector('button[id="tower"]');
+const buttonBot = document.querySelector('button[id="bot"]');
+const buttonAlgorithm = document.querySelector('button[id="algorithm"]');
+
+
+buttonTower.addEventListener('click', event => {
+    window.open("https://github.com/duck84/Machine-Learning")
+})
+
+
+buttonBot.addEventListener('click', event => {
+    window.open("https://github.com/duck84/Markov-Rust")
+})
+
+
+buttonAlgorithm.addEventListener('click', event => {
+    window.open("https://github.com/duck84/Algorithms")
+})
