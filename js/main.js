99eedbbc0e78a2e241b671518be492e3394627d6
Mike <mmcgrath@pdx.edu>
2019-10-31 09:59:21 -0700

    added contact form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const buttonTower = document.querySelector('button[id="tower"]');
 const buttonBot = document.querySelector('button[id="bot"]');
 const buttonAlgorithm = document.querySelector('button[id="algorithm"]');
-
+const buttonSubmit = document.getElementById('submit');
+const buttonSubmit = document.getElementById('reset');
+const inputFirst = document.getElementById('fname');
+const infputLast = document.getElementById('lname');
 
 buttonTower.addEventListener('click', event => {
     window.open("https://github.com/duck84/Machine-Learning")
@@ -15,4 +18,8 @@ buttonBot.addEventListener('click', event => {
 
 buttonAlgorithm.addEventListener('click', event => {
     window.open("https://github.com/duck84/Algorithms")
+})
+
+buttonSubmit.addEventListener('click', event => {
+
 })
